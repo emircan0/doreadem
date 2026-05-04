38ec61e6176887fb4aff8166c0c76ca0e355d2d7
Emircan Mert <emircanmertt@gmail.com>
2026-05-04 21:31:07 +0300

    Fix mobile horizontal scrolling issue by adding overflow-x-hidden

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -38,7 +38,7 @@ const App = () => {
         <NotificationProvider>
           <Router>
             <CartProvider>
-              <div className="flex flex-col min-h-screen">
+              <div className="flex flex-col min-h-screen overflow-x-hidden">
                   <ToastContainer 
                   position="bottom-right"
                   autoClose={3000}
